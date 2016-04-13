ec1fa8f0fd8a40ae846a32b1f9fc37f0d57dec3d
William Wu <william.wu.8156@gmail.com>
2016-04-12 17:49:30 -0700

    commit operation

---
+++ test/karma.conf.js
@@ -0,0 +1,28 @@
+module.exports = function(config){
+  config.set({
+
+    basePath : '../',
+
+    files : [
+      'app/angular/angular.js',
+      'test/unit/**/*.js'
+    ],
+
+    autoWatch : true,
+
+    frameworks: ['jasmine'],
+
+    browsers : ['Chrome'],
+
+    plugins : [
+            'karma-chrome-launcher',
+            'karma-jasmine'
+            ],
+
+    junitReporter : {
+      outputFile: 'test_out/unit.xml',
+      suite: 'unit'
+    }
+  });
+};
+
